ce963ec80fed07eab2a5648daa8e2923726116d7
Cortadoo <93486628+Cortadoo@users.noreply.github.com>
2022-12-27 09:28:34 +0000

    moving to folder

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,5 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: gr
+  -moz-osx-font-smoothing: grayscale;
+  text-align
