0a19326b7b7d071ce66388fac5161b7956006f1d
Cortadoo <93486628+Cortadoo@users.noreply.github.com>
2022-12-30 21:43:47 +0000

    Upload image

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <template>
   <div class="hello">
     <h1>TextToEncrypt</h1>
-    <input v-model="inputText" placeholder="Enter a string" /> <br
+    <input v-model="inputText" placeholder="Enter a string" /> <br /><br/>
+    <button 
