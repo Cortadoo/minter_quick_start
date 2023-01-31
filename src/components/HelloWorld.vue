5cafb3f9dbff5890958f7621be073bed1af196ee
Cortadoo <93486628+Cortadoo@users.noreply.github.com>
2023-01-31 11:31:50 +0000

    renaming project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,4 +27,4 @@ export default {  name: "aes-page",
           var encrypted = this.$CryptoJS.AES.encrypt(srcs, key, 
             {
               iv: iv,
-         
+              mode: this.$CryptoJ
